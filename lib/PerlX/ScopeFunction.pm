c485599be609e00f74359aa5552737ec88dbf8ca
Kang-min Liu <gugod@gugod.org>
2023-03-09 23:18:47 +0900

    doc.

--- lib/PerlX/ScopeFunction.pm
+++ lib/PerlX/ScopeFunction.pm
@@ -22,3 +22,72 @@ sub __rewrite_with {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+PerlX::ScopeFunction -- new keywords for creating scopes.
+
+=head1 SYNOPSIS
+
+Use C<with> keyword to constraint the use result of the given
+expression to a smaller lexical scope.
+
+    with ( grep { $_ % 2 == 0 } @input ) {
+        my @even_nums = @_;
+
+        say "There are " . scalar(@even_nums) . " even numbers";
+    }
+
+=head1 DESCRIPTION
+
+Scope functions are helper functions that helps to constraint the use
+of an given expression, to make the boundray look clearer, usually by
+making a inner lexical scope.
+
+=head2 C<with> (EXPR) { CODE }
+
+The C<with> keyword can be used to bring the result of an given EXPR
+to a smaller scope (code block):
+
+    with ( EXPR ) {
+        CODE
+    }
+
+The EXPR are evaluated in list context, and the result (a list) can be
+accessed via either C<@_> or C<$_>. C<@_> contains the entire list and
+C<$_> contains the last value of the list (C<$_[-1]>).
+
+=head1 AUTHOR
+
+Kang-min Liu  C<< <gugod@gugod.org> >>
+
+=head1 LICENCE
+
+The MIT License
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
